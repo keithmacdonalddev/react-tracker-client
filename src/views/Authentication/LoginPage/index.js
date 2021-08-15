49d95ed8e0c4e0556c89b67d51ec8be3a86f07d4
keithmacdonalddev <tenantbureau6@gmail.com>
2021-08-15 17:02:54 -0400

    clean up

--- src/views/Authentication/LoginPage/index.js
+++ src/views/Authentication/LoginPage/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
 import HomeButton from './HomeButton';
 import LoginForm from 'components/LoginForm';
-
 import classname from './LoginPage.module.css';
 
 const LoginPage = () => {
 	return (
-		<div className={classname.loginPageContainer}>
+		<div className={classname.login_page_container}>
 			<HomeButton />
 			<LoginForm />
 		</div>
--- src/views/Welcome/index.js
+++ src/views/Welcome/index.js
@@ -1,19 +1,14 @@
-import React from 'react';
-import SiteName from './SiteName'; // local component
-import HeaderText from './HeaderText'; // local component
-import StartButton from './StartButton'; // local component
-import classname from './Welcome.module.css'; // CSS
+import { Link } from 'react-router-dom';
+import classname from './Welcome.module.css';
 
-const HomePage = () => {
+export default function HomePage() {
 	return (
 		<div className={classname.welcome_page_container}>
-			<SiteName />
-			<div className={classname.content_container}>
-				<HeaderText />
-				<StartButton />
-			</div>
+			<div className={classname.site_name}>React Tracker</div>
+			<h4 className={classname.hero_text}>Portfolio tools designed for aspiring software engineers</h4>
+			<Link to={'/login'}>
+				<button className={classname.button}>Start for Free</button>
+			</Link>
 		</div>
 	);
-};
-
-export default HomePage;
+}
